dd1b0ad96cc97ef8046172e92ceffd881e07d88e
miki2525 <mikolaj.miki2525@gmail.com>
2022-12-06 09:29:53 +0100

    lab 5

---
+++ 5/src/main/java/org/tau/BankAccount.java
@@ -0,0 +1,83 @@
+package org.tau;
+
+
+public class BankAccount {
+
+    private String accno;
+    private String name;
+    private AccType acc_type;
+    private long balance;
+
+    private String owner;
+
+    public BankAccount(String accno, String name, AccType acc_type, long balance, String owner) {
+        this.accno = accno;
+        this.name = name;
+        this.acc_type = acc_type;
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public BankAccount() {}
+
+    //method to display account details
+    public String showAccount() {
+        return
+                "Name of account holder: " + name +
+                        ", Account no.: " + accno +
+                        ", Account type: " + acc_type +
+                        ", Balance: " + balance +
+                        ", Owner: " + owner;
+    }
+
+    //method to deposit money
+    public long deposit(long amount) {
+        if (amount > 0) {
+            balance = balance + amount;
+        } else {
+            throw new IllegalArgumentException("amount can't be 0 or less. Transaction Failed");
+        }
+        return balance;
+    }
+
+    //method to withdraw money
+    public long withdrawal(long amount) {
+        if (balance < amount && getAcc_type() == AccType.DEBIT) {
+            throw new IllegalStateException("balance is less than amount. Transaction failed");
+        } else if (amount <= 0) {
+            throw new IllegalStateException("amount is too small. Transaction failed");
+        } else {
+            balance = balance - amount;
+        }
+        return balance;
+    }
+
+    //method to search an account number
+    public boolean search(String ac_no) {
+        if (accno.equals(ac_no)) {
+            System.out.println(showAccount());
+            return (true);
+        }
+        return (false);
+    }
+
+    public String getAccno() {
+        return accno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public AccType getAcc_type() {
+        return acc_type;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+}
---
+++ 5/src/test/java/org/tau/BankAccountTest.java
@@ -0,0 +1,120 @@
+package org.tau;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class BankAccountTest {
+
+    @Mock
+    BankAccount bankAccount;
+
+    @Test
+    void shouldShowAccount() {
+        //
+        when(bankAccount.showAccount()).thenReturn("Name of account holder: SuperAccount" +
+                ", Account no.: 1234" +
+                ", Account type: DEBIT" +
+                ", Balance: 1000" +
+                ", Owner: John");
+        //
+        //
+        assertEquals(bankAccount.showAccount(), "Name of account holder: SuperAccount" +
+                ", Account no.: 1234" +
+                ", Account type: DEBIT"  +
+                ", Balance: 1000" +
+                ", Owner: John");
+    }
+
+    @Test
+    void shouldShowBalance() {
+        //
+        long expectedValue = 2000L;
+        when(bankAccount.getBalance()).thenReturn(2000L);
+        //
+        //
+        assertEquals(expectedValue,bankAccount.getBalance());
+    }
+
+    @Test
+    void shouldMakeADeposit() {
+        //
+        final long deposit = 500L;
+        final long expectedBalance = 1500L;
+        when(bankAccount.getBalance()).thenReturn(1500L);
+        when(bankAccount.deposit(deposit)).thenReturn(1500L);
+        //
+        long balanceAfterDeopsit= bankAccount.deposit(deposit);
+        //
+        assertEquals(expectedBalance, balanceAfterDeopsit);
+        assertEquals(balanceAfterDeopsit, bankAccount.getBalance());
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionDuringDeposit() {
+        //
+        when(bankAccount.deposit(anyLong())).thenThrow(IllegalArgumentException.class);
+        //
+        //
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.deposit(-10000));
+    }
+
+    @Test
+    void shouldMakeAWithdrawal() {
+        //
+        final long withdrawal = 500L;
+        final long expectedBalance = 1500L;
+        when(bankAccount.getBalance()).thenReturn(1500L);
+        when(bankAccount.withdrawal(withdrawal)).thenReturn(1500L);
+        //
+        long balanceAfterWitdrawal = bankAccount.withdrawal(withdrawal);
+        //
+        assertEquals(expectedBalance, balanceAfterWitdrawal);
+        assertEquals(balanceAfterWitdrawal, bankAccount.getBalance());
+    }
+
+    @Test
+    void shouldThrowBalanceIsLessThanAmountDuringWithrawal() {
+        //
+        final long veryBigLong = 9999999999L;
+        when(bankAccount.withdrawal(veryBigLong)).thenThrow(new IllegalStateException("balance is less than amount. Transaction failed"));
+        //
+        IllegalStateException ex = assertThrows(IllegalStateException.class, ()-> bankAccount.withdrawal(veryBigLong));
+        //
+        assertEquals(ex.getMessage(), "balance is less than amount. Transaction failed");
+    }
+
+    @Test
+    void shouldThrowAmountIsTooSmallDuringWithrawal() {
+        //
+        final long toSmallAmount = 0L;
+        when(bankAccount.withdrawal(toSmallAmount)).thenThrow(new IllegalStateException("amount is too small. Transaction failed"));
+        //
+        IllegalStateException ex = assertThrows(IllegalStateException.class, ()-> bankAccount.withdrawal(toSmallAmount));
+        //
+        assertEquals(ex.getMessage(), "amount is too small. Transaction failed");
+    }
+
+    @Test
+    void shouldFindBankAccount() {
+        //
+        when(bankAccount.search(anyString())).thenReturn(true);
+        //
+        //
+        assertTrue(bankAccount.search("1234"));
+    }
+
+    @Test
+    void shouldSucceed(){
+        assertTrue(true);
+    }
+}
